ccd07b6da03184160f21de055ea14dd90f8991e9
Ttiki <clem64.combier@gmail.com>
2022-01-20 09:24:36 +0100

    Lezgong on sauve du temps de ouf avec la classe MqttClient

--- src/main/java/app/AirData.java
+++ src/main/java/app/AirData.java
@@ -1,7 +1,10 @@
 package app;
 
-@DATA
+import lombok.Data;
+
+@Data
 public class AirData {
 	private int id;
 	private String name;
+	private 
 }
---
+++ src/main/java/app/Mqttclient.java
@@ -0,0 +1,12 @@
+package app;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class Mqttclient implements ServletContextListener {
+
+	private Mqttclient() {
+		
+	}
+}
